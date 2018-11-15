711154eb4b27b09954b9b63dfd1def6ea6adb399
Adam O'Neil <adamosoftware@gmail.com>
2018-11-15 14:30:51 -0500

    first commit

---
+++ SchemaSync.EF.Library/EntityFrameworkProvider.cs
@@ -0,0 +1,32 @@
+﻿using SchemaSync.Library.Interfaces;
+using SchemaSync.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace SchemaSync.EF.Library
+{
+	public class EntityFrameworkProvider : IDbAssemblyProvider
+	{
+		public ObjectTypeFlags ObjectTypes => ObjectTypeFlags.Tables | ObjectTypeFlags.ForeignKeys | ObjectTypeFlags.Schemas;
+
+		public Database GetDatabase(string path)
+		{
+			var assembly = Assembly.LoadFrom(path);
+			return GetDatabase(assembly);
+		}
+
+		public Database GetDatabase(Assembly assembly)
+		{
+			var types = assembly.GetExportedTypes();
+			var typeTableMap = GetTypeTableDictionary(types);
+
+			throw new NotImplementedException();
+		}
+
+		private Dictionary<Type, Table> GetTypeTableDictionary(Type[] types)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
